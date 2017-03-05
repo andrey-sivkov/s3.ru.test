da87372090d7c7a3269e228ce7d08f9c17bd1967
andrey-sivkov <sadko@yandex.ru>
2017-03-05 15:03:13 +0300

    1-st change

---
+++ includes/smarty/compile_dir/013f9c2663958b063aa05afad9650b499d51e012_0.file.index.tpl.php
@@ -0,0 +1,45 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-03-05 13:00:51
+  from "D:\xampp\htdocs\s3.ru.test\includes\smarty\templates\index.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_58bbfdf380f854_04407343',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '013f9c2663958b063aa05afad9650b499d51e012' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\s3.ru.test\\includes\\smarty\\templates\\index.tpl',
+      1 => 1488714133,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_58bbfdf380f854_04407343 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!doctype html>
+<html>
+<head>
+    <title>Products</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <link type="text/css" rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons" />
+    <?php echo '<script'; ?>
+ src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+</head>
+<body id="top" class="page">
+<div class="header"></div>
+<div class="content"></div>
+<div class="footer"></div>
+</body>
+</html><?php }
+}
--- includes/smarty/templates/index.tpl
+++ includes/smarty/templates/index.tpl
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons" />
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
-    <script src="/js/main.js"></script>
 </head>
 <body id="top" class="page">
 <div class="header"></div>
