82f6a1b673d1017d20b4c193635bbd3490975d27
andrey-sivkov <sadko@yandex.ru>
2017-03-05 14:41:33 +0300

    1-st change

---
+++ includes/smarty/compile_dir/%%CE^CE1^CE105C11%%index.tpl.php
@@ -0,0 +1,71 @@
+<?php /* Smarty version 2.6.22, created on 2017-03-05 12:23:33
+         compiled from D:/xampp/htdocs/s3.ru.test/includes/smarty/templates/index.tpl */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title><?php echo $this->_tpl_vars['metadata']['title']; ?>
+</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta name='yandex-verification' content='422d1d644eb4f024' />
+<meta name="keywords" content="<?php echo $this->_tpl_vars['metadata']['keywords']; ?>
+" />
+<meta name="description" content="<?php echo $this->_tpl_vars['metadata']['description']; ?>
+" />
+<link type="image/x-icon" rel="icon" href="/favicon.ico" />
+<link type="text/css" rel="stylesheet" href="/css/style.css" />
+<link type="text/css" rel="stylesheet" href="/css/stuck.css" />
+<link type="text/css" rel="stylesheet" href="/css/font-awesome.css" />
+<link type="text/css" rel="stylesheet" href="/css/touch_styles.css" />
+<link type="text/css" rel="stylesheet" href="/css/camera.css" />
+<link type="text/css" rel="stylesheet" href="/css/contact-form.css" media="screen" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+<script type="text/javascript" src="/js/jquery/jquery-1.3.2.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery-ui-1.7.3.custom.min.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.js"></script>
+<script type="text/javascript" src="/js/jquery/touchTouch.jquery.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery-migrate-1.2.1.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="/js/jquery/script.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.equalheights.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.ui.totop.js"></script>     
+<script type="text/javascript" src="/js/jquery/superfish.js"></script>
+<script type="text/javascript" src="/js/jquery/tmStickUp.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.mobilemenu.js"></script>
+<script type="text/javascript" src="/js/jquery/TMForm.js"></script>
+<script type="text/javascript" src="/js/jquery/modal.js"></script>
+<script type="text/javascript" src="/js/jquery/camera.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.maskedinput.min.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.cookie.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+<body id="top" class="page1">
+<!-- Header BOF-->
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "includes/tpl/header.tpl", 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+<!-- Header EOF-->
+<!-- MainPart BOF -->
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "includes/tpl/modules/".($this->_tpl_vars['module']).".tpl", 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+<!-- MainPart EOF -->
+<!-- Footer BOF -->
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "includes/tpl/footer.tpl", 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+<!-- Footer EOF -->
+<!-- Counters BOF -->
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "includes/tpl/counters.tpl", 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+<!-- Counters EOF -->
+</body>
+</html>
---
+++ includes/smarty/templates/index.tpl
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <title>Products</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <link type="text/css" rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/main.js"></script>
+</head>
+<body id="top" class="page">
+<div class="header"></div>
+<div class="content"></div>
+<div class="footer"></div>
+</body>
+</html>
