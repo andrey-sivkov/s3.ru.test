fe8b5eadcf65ed8d77abfd9a17783894459bc3ee
Andrey Sivkov <sadko@yandex.ru>
2021-07-05 08:12:33 +0300

    Update application_top.php

--- includes/application_top.php
+++ includes/application_top.php
@@ -6,21 +6,25 @@ error_reporting(E_ALL & ~E_NOTICE);
 session_start();
 
 // настройки
-require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/configure.php';
+require_once 'includes/configure.php';
 // основные функции
-require_once DIR_FS_CATALOG . '/includes/functions/general.php';
+require_once 'includes/functions/general.php';
+// класс Smarty
+require_once 'includes/classes/Smarty/Smarty.class.php';
+// класс DbSimbple
+require_once 'includes/classes/DbSimple/Generic.php';
 // класс Product
-require_once DIR_FS_CATALOG . '/includes/classes/Product.php';
+require_once 'includes/classes/Product.php';
 // класс Order
-require_once DIR_FS_CATALOG . '/includes/classes/Order.php';
+require_once 'includes/classes/Order.php';
 
 // инициализация класса Smarty
 $Smarty = new Smarty();
-$Smarty->setTemplateDir(DIR_FS_SMARTY . '/templates')
-  ->setCompileDir(DIR_FS_SMARTY . '/compile_dir')
+$Smarty->setTemplateDir('includes/smarty/templates')
+  ->setCompileDir('includes/smarty/compile_dir')
   ->setForceCompile(true);
 
 // установка соединения с БД
-$DB = DbSimple_Generic::connect('mysql://' . DB_SERVER_USERNAME . ':' . DB_SERVER_PASSWORD . '@' . DB_SERVER . '/' . DB_DATABASE);
+$DB = @DbSimple_Generic::connect('mysqli://' . DB_SERVER_USERNAME . ':' . DB_SERVER_PASSWORD . '@' . DB_SERVER . '/' . DB_DATABASE);
 if ($DB->error['code'])
   die($DB->error['message']);
